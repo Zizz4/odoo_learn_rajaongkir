3aab71a7bc7ba6368d72668b38e64817bf85cd1e
Azizz <syahrilazis2005@gmail.com>
2022-10-12 17:23:45 +0700

    Make a new button in SO form

---
+++ custom_addons/Sales_T&C_Replace/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'SO T&C Replace',
+    'version': '1.0',
+    'summary': 'Generate the name and phone number from the user',
+    'description': "Generate the name and phone number from the user",
+    'website': 'https://www.ecosains.com',
+    'depends': ['sale'],
+    'category': 'technical',
+    'sequence': 15,
+    'data': [
+        'views/sale_views_refresh_note.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
